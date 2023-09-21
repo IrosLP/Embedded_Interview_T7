b3fe10ad45d582a6b975f7a3e54fa4a085068e03
Phạm Văn Lộc <96722641+IrosLP@users.noreply.github.com>
2023-09-21 13:09:48 +0700

    Add files via upload

---
+++ Homework/Embedded/SPI/Transmit Array/SlaveReceiveArray/SlaveReceiveArray.c
@@ -0,0 +1,56 @@
+//#include "stm32f10x.h"
+#include "stm32f10x_gpio.h"
+#include "stm32f10x_rcc.h"
+
+#define SCK		GPIO_Pin_6
+#define MOSI	GPIO_Pin_7
+#define SS		GPIO_Pin_8
+#define PORT	GPIOB
+
+void config(void);
+void check(void);
+uint8_t receive(void);
+void receiveArray(uint8_t array[], uint8_t length);
+
+int main(){
+	uint8_t array[] = {0, 22, 0, 0, 0, 55, 0, 0, 0, 0};
+	config();
+	while(1){
+		receiveArray(array, 10);
+	}
+	return 0;
+}
+
+void config(){
+	GPIO_InitTypeDef gpio;
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+	
+	gpio.GPIO_Pin = SCK | MOSI;
+	gpio.GPIO_Mode = GPIO_Mode_IPD;
+	gpio.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(PORT, &gpio);
+	
+	gpio.GPIO_Pin = SS;
+	gpio.GPIO_Mode = GPIO_Mode_IPU;
+	gpio.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(PORT, &gpio);
+}
+
+uint8_t receive(){
+	uint8_t data = 0x00;
+	while(GPIO_ReadInputDataBit(PORT, SS) == 1);
+	for(uint8_t i=0; i<8; i++){
+		while(GPIO_ReadInputDataBit(PORT, SCK) == 0);
+		if(GPIO_ReadInputDataBit(PORT, MOSI))	data |= (0x01<<i);
+		while(GPIO_ReadInputDataBit(PORT, SCK) == 1);
+	}
+	while(GPIO_ReadInputDataBit(PORT, SS) == 0);
+	return data;
+}
+
+void receiveArray(uint8_t array[], uint8_t length){
+	while(GPIO_ReadInputDataBit(PORT, SS) == 1);
+	for(uint8_t i=0; i<length; i++)
+		array[i] = receive();
+}
+		
